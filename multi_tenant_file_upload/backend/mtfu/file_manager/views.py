55a10f00ea3440f1c04ca634a0d30c7f633167e8
Vu Hoang Giang <itworkjp@gmail.com>
2023-05-11 17:22:08 +0700

    simplify upload file name store in S3

--- multi_tenant_file_upload/backend/mtfu/file_manager/views.py
+++ multi_tenant_file_upload/backend/mtfu/file_manager/views.py
@@ -11,6 +11,7 @@ from mtfu.file_manager.models import File
 import datetime
 from django.db import transaction
 
+
 class UploadView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -23,7 +24,7 @@ class UploadView(APIView):
         resource_id = request.data["resource_id"]
 
         # Uploading the image to S3
-        filename_versioning = versioning_filename(filename=upload_file.name)
+        filename = upload_file.name
         s3_client = boto3.client(
             "s3",
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
@@ -34,31 +35,24 @@ class UploadView(APIView):
             s3_client.upload_fileobj(
                 upload_file,
                 settings.AWS_STORAGE_BUCKET_NAME,
-                settings.ASSET_IMAGE_FOLDER + "/" + filename_versioning,
+                settings.ASSET_IMAGE_FOLDER + "/" + filename,
             )
         except ClientError as e:
             return
-        
+
         # get user from session
         user = request.user
 
         tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
 
         file = File(
-            name=filename_versioning,
+            name=filename,
             resource=resource,
             resource_id=resource_id,
             tenant=user,
-            location=settings.ASSET_IMAGE_FOLDER + "/" + filename_versioning,
-            expire_at=tomorrow
+            location=settings.ASSET_IMAGE_FOLDER + "/" + filename,
+            expire_at=tomorrow,
         )
         file.save()
 
         return Response({"message": "File uploaded successfully"})
-
-
-def versioning_filename(filename):
-    ext = filename.split(".")[-1]
-    name = filename.split(".")[0]
-    filename = "{}-{}.{}".format(name, uuid.uuid1().hex[:8], ext)
-    return filename
