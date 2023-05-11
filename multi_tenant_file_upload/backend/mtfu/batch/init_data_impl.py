c794d30e42bc92b82cac254896e39deb5b097d69
Vu Hoang Giang <itworkjp@gmail.com>
2023-05-12 00:34:51 +0700

    fix create tenants

--- multi_tenant_file_upload/backend/mtfu/batch/init_data_impl.py
+++ multi_tenant_file_upload/backend/mtfu/batch/init_data_impl.py
@@ -1,31 +1,20 @@
 from mtfu.auth_user.models import Tenant
 from django.db.models import Q
-from bulk_sync import bulk_sync
 
 
 def copy_to_postgresql():
-    print("Copying data to PostgreSQL")
     create_tenants()
 
 
 def create_tenants():
-    users = []
-
     for i in range(1, 11):
-        user = Tenant(username="john" + str(i))
-        user.set_password("mypassword")
-        users.append(user)
+        username = "john" + str(i)
+        
+        # check if user exists
+        if Tenant.objects.filter(Q(username=username)).exists():
+            continue
 
-    filters = Q()
-    key_fields = ("username",)
-    ret = bulk_sync(
-        new_models=users,
-        filters=filters,
-        key_fields=key_fields,
-        skip_deletes=True,
-        skip_updates=False,
-    )
-
-    print(
-        "Results of bulk_sync users: " "{created} created, {updated} updated, {deleted} deleted.".format(**ret["stats"])
-    )
+        Tenant.create(
+            username=username,
+            password="test"
+        )
