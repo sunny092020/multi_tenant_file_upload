76c22338468b943f4a60501e7c8842a0bf520487
Vu Hoang Giang <itworkjp@gmail.com>
2023-05-11 17:22:27 +0700

    add more assert

--- multi_tenant_file_upload/backend/mtfu/tests/test_upload.py
+++ multi_tenant_file_upload/backend/mtfu/tests/test_upload.py
@@ -8,7 +8,8 @@ django.setup()
 
 from rest_framework.test import APIClient
 from mtfu.auth_user.models import Tenant
-
+from mtfu.file_manager.models import File
+import datetime
 
 @pytest.fixture
 def api_client():
@@ -36,13 +37,31 @@ def api_client():
 
 def test_upload_with_authorized_user(api_client):
     fileUploadPath = os.path.join(os.path.dirname(__file__), "test_file")
+
+    # verify no file record exists
+    assert File.objects.count() == 0
+
     with open(fileUploadPath, "rb") as file:
-        response = api_client.post("/api/upload", {
-            "file": file,
-            "resource": "product",
-            "resource_id": 1,
-        })
+        response = api_client.post(
+            "/api/upload",
+            {
+                "file": file,
+                "resource": "product",
+                "resource_id": 1,
+            },
+        )
         assert response.status_code == 200
+
+    # verify file record exists
+    assert (
+        File.objects.filter(
+            name="test_file",
+            resource="product",
+            resource_id=1,
+            tenant__username="john1",
+        ).count()
+        == 1
+    )
 
 
 def test_upload_with_unauthorized_user():
