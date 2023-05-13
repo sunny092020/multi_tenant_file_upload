574db0c54b86852d5803e951e3aabbdfb686b550
Vu Hoang Giang <itworkjp@gmail.com>
2023-05-13 09:36:04 +0700

    install jq

--- multi_tenant_file_upload/scripts/install_env.sh
+++ multi_tenant_file_upload/scripts/install_env.sh
@@ -7,7 +7,8 @@ apt-get -y install \
     curl \
     gnupg \
     lsb-release \
-    awscli
+    awscli \
+    jq
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
